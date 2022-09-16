fd9e82c33f1dcb1ba2acd7df976f423e8bac60f6
Alexandro Valdez <alexandroivaldez@gmail.com>
2022-09-15 19:24:00 -0500

    Mapped Object array to <div>

--- retro-mart/src/App.js
+++ retro-mart/src/App.js
@@ -1,13 +1,36 @@
 
 const App = () => {
+
+  const categories = [
+    {
+      title: 'Hats',
+      id: 1
+    },{
+      title: 'Jackets',
+      id: 2
+    },{
+      title: 'Sneakers',
+      id: 3
+    },{
+      title: 'Womens',
+      id: 4
+    },{
+      title: 'Mens',
+      id: 5
+    }
+  ]
+
   return (
     <div className="categories-container">
-      <div className="category-container">
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
+      {categories.map(({title}) => (
+        <div className="category-container">
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
       </div>
+      ))}
     </div>
   );
 }
