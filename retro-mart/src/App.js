bf0592c0cbeba5154106184fc788ecfa99c77e53
Alexandro Valdez <alexandroivaldez@gmail.com>
2022-09-15 22:10:03 -0500

    Added Outlet from React-Router-Dom

--- retro-mart/src/App.js
+++ retro-mart/src/App.js
@@ -1,10 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from "./routes/home/home.component.jsx";
+import { Outlet } from 'react-router-dom';
+
+const Shop = () => {
+  return (
+    <h1>SHOP</h1>
+  );
+}
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />}/>
+      <Route path='/home' element={<Home />}>
+        <Route path='shop' element={<Shop />}/>
+      </Route>
     </Routes>
   );
 }
--- retro-mart/src/components/directory/directory.component.jsx
+++ retro-mart/src/components/directory/directory.component.jsx
@@ -1,4 +1,5 @@
 import CategoryItem from "../category-item/category-item.component";
+import { Outlet } from 'react-router-dom';
 import "./directory.styles.scss";
 
 const Directory = ({categories}) => {
